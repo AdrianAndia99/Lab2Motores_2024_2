eefabbb743c3436527a57bae038a82d347998b2f
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2025-04-14 16:51:00 -0500

    ga

--- Assets/Scripts/CambioEscena.cs
+++ Assets/Scripts/CambioEscena.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CambioEscena : MonoBehaviour
 {
+    public TextMeshProUGUI textSample;
+
     public void Cambio(string nombre)
     {
         SceneManager.LoadScene(nombre);
         Time.timeScale = 1f;
     }
 
+    public void ChangeText(string text)
+    {
+        textSample.text = text;
+    }
     public void Restart()
     {
         SceneManager.LoadScene("MemoryCards");
